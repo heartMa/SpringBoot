d1ff2a3ab622636d726f2a0443a3f07680615850
2zheteng <netheartma@gmail.com>
2019-07-16 13:45:13 +0800

    配置讲解

--- 14-SpringBootMybatis/src/main/java/com/w3cjava/dao/UserMapper.java
+++ 14-SpringBootMybatis/src/main/java/com/w3cjava/dao/UserMapper.java
@@ -1,19 +1,73 @@
 package com.w3cjava.dao;
 
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.w3cjava.entity.User;
 
 @Mapper
 public interface UserMapper {
 
+	/**
+	 * 
+	 * @author cos
+	 * @desc 使用@Param传参，@Param中定义的name对应了SQL中的#{name}，age对应了SQL中的#{age}
+	 * @param name
+	 * @return
+	 */
     @Select("SELECT * FROM USER WHERE NAME = #{name}")
     User findByName(@Param("name") String name);
 
     @Insert("INSERT INTO USER(NAME, AGE) VALUES(#{name}, #{age})")
     int insert(@Param("name") String name, @Param("age") Integer age);
 
+    /**
+     * 
+     * @author cos
+     * @desc 通过Map<String, Object>对象来作为传递参数的容器
+     * @param map
+     * @return
+     */
+    @Insert("INSERT INTO USER(NAME, AGE) VALUES(#{name,jdbcType=VARCHAR}, #{age,jdbcType=INTEGER})")
+    int insertByMap(Map<String, Object> map);
+    
+    /**
+     * 
+     * @author cos
+     * @desc 使用对象
+     * @param user
+     * @return
+     */
+    
+    @Insert("INSERT INTO USER(NAME, AGE) VALUES(#{name}, #{age})")
+    int insertByUser(User user);
+    
+    @Update("UPDATE user SET age=#{age} WHERE name=#{name}")
+    void update(User user);
+
+    @Delete("DELETE FROM user WHERE id =#{id}")
+    void delete(Long id);
+    
+    
+    /**
+     * 
+     * @author cos
+     * @desc 返回结果的绑定
+     * @return
+     */
+    @Results({
+        @Result(property = "name", column = "name"),
+        @Result(property = "age", column = "age")
+    })
+    @Select("SELECT name, age FROM user")
+    List<User> findAll();
 }
--- 14-SpringBootMybatis/src/test/java/com/w3cjava/dao/UserMapperTest.java
+++ 14-SpringBootMybatis/src/test/java/com/w3cjava/dao/UserMapperTest.java
@@ -1,5 +1,9 @@
 package com.w3cjava.dao;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,5 +27,43 @@ public class UserMapperTest {
 		userMapper.insert("AAA", 20);
 		User u = userMapper.findByName("AAA");
 		Assert.assertEquals(20, u.getAge().intValue());
+	}
+	
+	@Test
+	public void testMap() throws Exception{
+		Map<String, Object> map = new HashMap<>();
+		map.put("name", "CCC");
+		map.put("age", 40);
+		userMapper.insertByMap(map);
+	}
+	
+	
+	@Test
+	@Rollback
+	public void testUserMapper() throws Exception {
+		// insert一条数据，并select出来验证
+		userMapper.insert("AAA", 20);
+		User u = userMapper.findByName("AAA");
+		Assert.assertEquals(20, u.getAge().intValue());
+		// update一条数据，并select出来验证
+		u.setAge(30);
+		userMapper.update(u);
+		u = userMapper.findByName("AAA");
+		Assert.assertEquals(30, u.getAge().intValue());
+		// 删除这条数据，并select验证
+		userMapper.delete(u.getId());
+		u = userMapper.findByName("AAA");
+		Assert.assertEquals(null, u);
+	}
+	
+	
+	@Test
+	@Rollback
+	public void testSelectMapper() throws Exception {
+		List<User> userList = userMapper.findAll();
+		for(User user : userList) {
+			Assert.assertEquals(null, user.getId());
+			Assert.assertNotEquals(null, user.getName());
+		}
 	}
 }
