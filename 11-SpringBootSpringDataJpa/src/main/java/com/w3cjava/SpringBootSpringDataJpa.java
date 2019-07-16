99dbd7359a993a4d738610c29ffefe941c0cdee0
2zheteng <netheartma@gmail.com>
2019-07-16 08:39:50 +0800

    SpringDataJpa

---
+++ 11-SpringBootSpringDataJpa/src/main/java/com/w3cjava/SpringBootSpringDataJpa.java
@@ -0,0 +1,18 @@
+package com.w3cjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * 
+ * @class  SpringBootSpringDataJpa
+ * @version TODO
+ * @author cos
+ * @desc   SpringDataJpa
+ *
+ */
+@SpringBootApplication
+public class SpringBootSpringDataJpa {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSpringDataJpa.class, args);
+	}
+}
