de2ac6ac8f22787d36e72e903a1733d4fd604272
2zheteng <netheartma@gmail.com>
2019-07-16 15:49:59 +0800

    AopWebLog

---
+++ 21-SpringBootAOPWebLog/src/main/java/com/w3cjava/SpringBootAopWebLog.java
@@ -0,0 +1,19 @@
+package com.w3cjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+/**
+ * 
+ * @class  SpringBootAopWebLog
+ * @version TODO
+ * @author cos
+ * @desc   AopWebLog
+ *
+ */
+@SpringBootApplication
+public class SpringBootAopWebLog {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAopWebLog.class, args);
+	}
+}
---
+++ 21-SpringBootAOPWebLog/src/test/java/com/w3cjava/service/UserServiceTest.java
@@ -0,0 +1,16 @@
+package com.w3cjava.service;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.cache.CacheManager;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.w3cjava.SpringBootAopWebLog;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(SpringBootAopWebLog.class)
+public class UserServiceTest {
+}
