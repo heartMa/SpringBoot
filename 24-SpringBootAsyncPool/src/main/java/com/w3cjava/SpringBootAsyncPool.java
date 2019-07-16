2a87f6f4fcd8feb71182fb5b3dcf7647a722e9c2
2zheteng <netheartma@gmail.com>
2019-07-16 17:07:15 +0800

    Pool and scheduledTaskExcutor

---
+++ 24-SpringBootAsyncPool/src/main/java/com/w3cjava/SpringBootAsyncPool.java
@@ -0,0 +1,12 @@
+package com.w3cjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringBootAsyncPool {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAsyncPool.class, args);
+	}
+}
