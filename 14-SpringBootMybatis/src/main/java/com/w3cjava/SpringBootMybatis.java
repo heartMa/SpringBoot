4bd093b67caa0f723585f881f6dbb227e8ccd9f5
2zheteng <netheartma@gmail.com>
2019-07-16 13:36:24 +0800

    SpringBootMybatis整合

---
+++ 14-SpringBootMybatis/src/main/java/com/w3cjava/SpringBootMybatis.java
@@ -0,0 +1,18 @@
+package com.w3cjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * 
+ * @class  SpringBootMybatis
+ * @version 1.0.0
+ * @author cos
+ * @desc   Mybatis
+ *
+ */
+@SpringBootApplication
+public class SpringBootMybatis {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMybatis.class, args);
+	}
+}
---
+++ 14-SpringBootMybatis/src/test/java/com/w3cjava/dao/UserMapperTest.java
@@ -0,0 +1,27 @@
+package com.w3cjava.dao;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.w3cjava.SpringBootMybatis;
+import com.w3cjava.entity.User;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringBootMybatis.class)
+public class UserMapperTest {
+	@Autowired
+	private UserMapper userMapper;
+
+	@Test
+	@Rollback
+	public void findByName() throws Exception {
+		userMapper.insert("AAA", 20);
+		User u = userMapper.findByName("AAA");
+		Assert.assertEquals(20, u.getAge().intValue());
+	}
+}
